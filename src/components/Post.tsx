fab7c0394a3bed0dd971d88cd9045866dc0e6fa9
peachbranch777 <peachbranch.0517@gmail.com>
2022-03-12 17:56:11 +0900

    feat: 🎸 add feature of comment

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -18,7 +18,67 @@ type Props = {
   username: string;
 };
 
+type Comment = {
+  id: string;
+  avatar: string;
+  text: string;
+  timestamp: any;
+  username: string;
+};
+
+const useStyles = makeStyles((theme) => ({
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+    marginRight: theme.spacing(1),
+  },
+}));
+
 const Post: React.FC<Props> = (props) => {
+  const classes = useStyles();
+  const user = useSelector(selectUser);
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState<Comment[]>([
+    {
+      id: "",
+      avatar: "",
+      text: "",
+      timestamp: null,
+      username: "",
+    },
+  ]);
+  const [openComments, setOpenComments] = useState(false);
+  useEffect(() => {
+    const unSub = db
+      .collection("posts")
+      .doc(props.postId)
+      .collection("comments")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setComments(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            avatar: doc.data().avatar,
+            text: doc.data().text,
+            username: doc.data().username,
+            timestamp: doc.data().timestamp,
+          }))
+        );
+      });
+    return () => {
+      unSub();
+    };
+  }, [props.postId]);
+  const newComment = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    db.collection("posts").doc(props.postId).collection("comments").add({
+      avatar: user.photoUrl,
+      text: comment,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      username: user.displayName,
+    });
+    setComment("");
+  };
   return (
     <div className={styles.post}>
       <div className={styles.post_avatar}>
@@ -43,6 +103,42 @@ const Post: React.FC<Props> = (props) => {
             <img src={props.image} alt="tweet" />
           </div>
         )}
+        <MessageIcon onClick={() => setOpenComments(!openComments)} />
+        {openComments && (
+          <>
+            {comments.map((com) => (
+              <div key={com.id} className={styles.post_comment}>
+                <Avatar src={com.avatar} className={classes.small} />
+                <span className={styles.post_commentUser}>@{com.username}</span>
+                <span className={styles.post_commentText}>{com.text}</span>
+                <span className={styles.post_headerTime}>
+                  {new Date(com.timestamp?.toDate()).toLocaleString()}
+                </span>
+              </div>
+            ))}
+          </>
+        )}
+        <form onSubmit={newComment}>
+          <div className={styles.post_form}>
+            <input
+              className={styles.post_input}
+              type="text"
+              placeholder="Type new comment ..."
+              value={comment}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setComment(e.target.value)
+              }
+            />
+            <button
+              className={
+                comment ? styles.post_button : styles.post_buttonDisable
+              }
+              type="submit"
+            >
+              <SendIcon className={styles.post_sendIcon} />
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
